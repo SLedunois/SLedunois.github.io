e03a6847a5c9850d8eab5c5782b4585e5d6e286b
LEDUNOIS Simon <simon.ledunois@gmail.com>
2020-04-07 09:42:52 +0200

    Fix(Header) transform header to React component

--- src/header/header.js
+++ src/header/header.js
@@ -9,39 +9,58 @@ import accounts from "../../data/accounts.json"
 import github from "../images/github.svg"
 import twitter from "../images/twitter.svg"
 
-const Header = ({ siteTitle }) => {
-  const currentPath =
-    typeof window !== `undefined` ? document.location.pathname : ""
-  return (
-    <header className="mb-20 pt-12">
-      <nav className="justify-between flex">
-        <ul className="flex justify-around font-medium">
-          <li className={currentPath === "/" && "active"}>
-            <Link to="/">Home</Link>
-          </li>
-          <li className={currentPath === "/about" && "active"}>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
-        <ul className="flex justify-around font-medium">
-          <li>
-            <a href={accounts.github} target="_blank" rel="noopener noreferrer">
-              <img src={github} alt="Github" />
-            </a>
-          </li>
-          <li>
-            <a
-              href={accounts.twitter}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img src={twitter} alt="Twitter" />
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  )
+class Header extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      currentPath: "",
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      currentPath:
+        typeof window !== `undefined` ? document.location.pathname : "",
+    })
+  }
+
+  render() {
+    const { currentPath } = this.state
+    return (
+      <header className="mb-20 pt-12">
+        <nav className="justify-between flex">
+          <ul className="flex justify-around font-medium">
+            <li className={currentPath === "/" && "active"}>
+              <Link to="/">Home</Link>
+            </li>
+            <li className={currentPath === "/about" && "active"}>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+          <ul className="flex justify-around font-medium">
+            <li>
+              <a
+                href={accounts.github}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={github} alt="Github" />
+              </a>
+            </li>
+            <li>
+              <a
+                href={accounts.twitter}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={twitter} alt="Twitter" />
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    )
+  }
 }
 
 Header.propTypes = {
