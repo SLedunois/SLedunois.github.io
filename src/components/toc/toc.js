b42003457ddba36d5722aff597e838ed274e4033
LEDUNOIS Simon <simon.ledunois@gmail.com>
2020-04-12 18:51:23 +0200

    feat(TOC) Add toc style

--- src/components/toc/toc.js
+++ src/components/toc/toc.js
@@ -1,11 +1,16 @@
 import React from "react"
 
+import "./toc.css"
+
 export default ({ headings }) => (
-  <ul>
-    {headings.map(heading => (
-      <a href={heading.url}>
-        <li key={heading.url}>{heading.title}</li>
-      </a>
-    ))}
-  </ul>
+  <div className="table-of-contents mt-8 mb-8">
+    <h2 className="text-lg font-extrabold">Table of contents</h2>
+    <ul>
+      {headings.map(heading => (
+        <a href={heading.url}>
+          <li key={heading.url}>{heading.title}</li>
+        </a>
+      ))}
+    </ul>
+  </div>
 )
