e6687f7015dea67ca109274e2729f8adf2362ba1
LEDUNOIS Simon <simon.ledunois@gmail.com>
2020-04-13 19:20:11 +0200

    fix(DesignSystem) Fix anchor links

--- src/components/design-system/design-system.js
+++ src/components/design-system/design-system.js
@@ -1,13 +1,21 @@
 import React from "react"
 
-const H1 = ({ children }) => (
-  <h1 className="text-3xl font-extrabold mt-4 mb-4">{children}</h1>
+const H1 = props => (
+  <h1 className="text-3xl font-extrabold mt-4 mb-4" {...props}>
+    {props.children}
+  </h1>
 )
 
-const H2 = ({ children }) => <h2 className="text-2xl font-bold">{children}</h2>
+const H2 = props => (
+  <h2 className="text-2xl font-bold mt-3 mb-3" {...props}>
+    {props.children}
+  </h2>
+)
 
-const H3 = ({ children }) => (
-  <h3 className="text-xl font-semibold">{children}</h3>
+const H3 = props => (
+  <h3 className="text-xl font-semibold mt-2 mb-2" {...props}>
+    {props.children}
+  </h3>
 )
 
 export default {
