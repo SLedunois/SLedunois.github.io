8ad07e3900d3e6372c5b696321275f668bd94865
LEDUNOIS Simon <simon.ledunois@gmail.com>
2020-04-16 18:04:41 +0200

    Feat(Code) Adding syntaxic color

--- src/components/design-system/design-system.js
+++ src/components/design-system/design-system.js
@@ -1,4 +1,6 @@
 import React from "react"
+import Highlight, { defaultProps } from "prism-react-renderer"
+import theme from "../../styles/monokai"
 
 const H1 = props => (
   <h1 className="text-3xl font-extrabold mt-4 mb-4" {...props}>
@@ -18,8 +20,38 @@ const H3 = props => (
   </h3>
 )
 
+const Pre = props => {
+  const className = props.children.props.className || ""
+  const matches = className.match(/language-(?<lang>.*)/)
+  return (
+    <Highlight
+      {...defaultProps}
+      code={props.children.props.children.trim()}
+      language={
+        matches && matches.groups && matches.groups.lang
+          ? matches.groups.lang
+          : ""
+      }
+      theme={theme}
+    >
+      {({ className, style, tokens, getLineProps, getTokenProps }) => (
+        <pre className={`p-8 rounded-lg ${className}`} style={style}>
+          {tokens.map((line, i) => (
+            <div {...getLineProps({ line, key: i })}>
+              {line.map((token, key) => (
+                <span {...getTokenProps({ token, key })} />
+              ))}
+            </div>
+          ))}
+        </pre>
+      )}
+    </Highlight>
+  )
+}
+
 export default {
   h1: H1,
   h2: H2,
   h3: H3,
+  pre: Pre,
 }
