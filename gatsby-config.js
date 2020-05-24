89affc8a197f388db2d43b771e3cca53c8735b82
LEDUNOIS Simon <simon.ledunois@gmail.com>
2020-05-24 14:13:56 +0200

    fix() Remove README + change manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -114,11 +114,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `SLedunois`,
+        short_name: `SLedunois`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#ffcc00`,
+        theme_color: `#ffcc00`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
