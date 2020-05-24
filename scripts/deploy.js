d4c4b1290fb8bf42bc6f39fd1ce1ffae0a97b868
LEDUNOIS Simon <simon.ledunois@gmail.com>
2020-05-24 17:00:46 +0200

    Fix(CI) Fix deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,9 +7,9 @@ const deploy = async () => {
     console.log("Building...")
     await execa("npm", ["run", "build"])
     // Understand if it's dist or build folder
-    const folderName = fs.existsSync("dist") ? "dist" : "build"
-    await execa("git", ["--worktree", folderName, "add", "--all"])
-    await execa("git", ["--worktree", folderName, "commit", "-m", "gh-pages"])
+    const folderName = "public"
+    await execa("git", ["--work-tree", folderName, "add", "--all"])
+    await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"])
     console.log("Pushing to gh-pages...")
     await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"])
     await execa("rm", ["-r", folderName])
