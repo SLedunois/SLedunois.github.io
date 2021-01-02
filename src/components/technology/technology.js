f7ce635f8b09f6875f3a171973ee18dfe1a16444
SLedunois <SLedunois@users.noreply.github.com>
2021-01-02 18:16:57 +0000

    deploy: 2774e52c452a947313f6eda3e3d1e2c3190e4e2b

--- src/components/technology/technology.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-
-const Technology = ({ name }) => {
-  const img = require(`../../images/technologies/${name}.svg`)
-  return <img src={img} alt={name} className="mr-4 mt-1 mb-2 h-6" />
-}
-
-export default Technology
